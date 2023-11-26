b104eb14f97de2a2d3aaa0671ab4c96b19d3fc68
Dmytro Vozniuk <dmytro.vozniuk@knu.com>
2023-11-26 19:47:55 +0200

    add-lab-5

---
+++ lab5.var3.py
@@ -0,0 +1,17 @@
+# lab 5
+# var 3
+# Виведення всіх абзаців, що містять у собі введений користувачем тег.
+
+with open(r"D:\Программирование\5\Lab-works\text.txt", "r", encoding="utf-8") as file:
+    file_read = file.read()
+while True:
+    tag = input("Введіть ваш тег:\n")
+    if "#" + tag not in file_read:     
+        print("Помилка!Такого тегу немає!\nСпробуйте ще раз!")
+    else:
+        break
+spleated_paragraphs = file_read.split("\n\n")
+for paragraph in spleated_paragraphs:
+    if "#" + tag in paragraph:
+        print(paragraph)
+       
